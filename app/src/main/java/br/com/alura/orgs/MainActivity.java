8bc33a1126ed93e972608cd21290371532c479f7
kariello <kariello@gmail.com>
2023-07-06 14:09:39 -0300

    v1

--- app/src/main/java/br/com/alura/orgs/MainActivity.java
+++ app/src/main/java/br/com/alura/orgs/MainActivity.java
@@ -1,8 +1,9 @@
 package br.com.alura.orgs;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Olá Mundo", Toast.LENGTH_SHORT).show();
     }
 }
