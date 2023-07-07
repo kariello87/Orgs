4ac7d1e18f7f8efe79380f2a0cda038788f96931
kariello <kariello@gmail.com>
2023-07-07 16:12:10 -0300

    v1

--- app/src/main/java/br/com/alura/orgs/MainActivity.java
+++ app/src/main/java/br/com/alura/orgs/MainActivity.java
@@ -5,12 +5,42 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import br.com.alura.orgs.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //jeito antigo de fazer
+        /*TextView viewNome = findViewById(R.id.nome);
+        viewNome.setText("Cesta de Frutas");*/
+
+        //JEITO NOVO DE FAZER
+
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        binding.nome.setText("Cesta de Frutas");
+        binding.descricao.setText("maçã, limão, pêra");
+        binding.valor.setText("R$ 5,99");
+
+
+        setContentView(binding.getRoot());
+
         Toast.makeText(this, "Olá Mundo", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
+
     }
 }
