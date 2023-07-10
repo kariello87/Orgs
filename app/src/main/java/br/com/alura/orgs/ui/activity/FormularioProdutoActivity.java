d7a1717651d4f35d135b02fcf897f3deb0b51a9f
kariello <kariello@gmail.com>
2023-07-10 15:35:43 -0300

    v1

--- app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.java
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.java
@@ -1,14 +1,23 @@
 package br.com.alura.orgs.ui.activity;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import br.com.alura.orgs.R;
+import br.com.alura.orgs.databinding.ActivityFormularioProdutoBinding;
 
 public class FormularioProdutoActivity extends AppCompatActivity {
 
-    public FormularioProdutoActivity() {
-        super(R.layout.activity_formulario_produto);
+    private ActivityFormularioProdutoBinding formularioProdutoBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_produto);
+        String nome = formularioProdutoBinding.editNome.getText().toString();
+        Log.i("nome", nome);
+
     }
-
-
 }
