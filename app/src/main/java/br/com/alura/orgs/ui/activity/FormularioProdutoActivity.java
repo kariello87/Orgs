17ef4cc3f8bda1ea0c0fa33ae66c5aa503f1fdb2
kariello <kariello@gmail.com>
2023-07-10 13:53:00 -0300

    v1

---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.java
@@ -0,0 +1,14 @@
+package br.com.alura.orgs.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.com.alura.orgs.R;
+
+public class FormularioProdutoActivity extends AppCompatActivity {
+
+    public FormularioProdutoActivity() {
+        super(R.layout.activity_formulario_produto);
+    }
+
+
+}
