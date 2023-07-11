648cbc72a71cb41db3596592d863387f67536e82
kariello <kariello@gmail.com>
2023-07-11 14:58:34 -0300

    v1

--- app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.java
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.java
@@ -1,12 +1,16 @@
 package br.com.alura.orgs.ui.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.math.BigDecimal;
+
 import br.com.alura.orgs.R;
 import br.com.alura.orgs.databinding.ActivityFormularioProdutoBinding;
+import br.com.alura.orgs.model.Produto;
 
 public class FormularioProdutoActivity extends AppCompatActivity {
     private ActivityFormularioProdutoBinding binding;
@@ -19,8 +23,22 @@ public class FormularioProdutoActivity extends AppCompatActivity {
 
         binding = ActivityFormularioProdutoBinding.inflate(getLayoutInflater());
         binding.btSalvar.setOnClickListener(view -> {
-            String nome2 = binding.editNome.getText().toString();
-            Toast.makeText(this, nome2, Toast.LENGTH_SHORT).show();
+            String nome = binding.editNome.getText().toString();
+            String descricao = binding.editDescricao.getText().toString();
+            String precoString = binding.editPreco.getText().toString();
+            BigDecimal preco;
+
+            if (precoString.isBlank()) {
+                preco = BigDecimal.ZERO;
+            } else {
+                preco = new BigDecimal(precoString);
+            }
+
+            Produto produto = new Produto(nome, descricao, preco);
+
+            Log.i("Produto", produto.toString());
+            Toast.makeText(this, nome, Toast.LENGTH_SHORT).show();
+
         });
 
         setContentView(binding.getRoot());
